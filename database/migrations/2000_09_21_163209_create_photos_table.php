c8c014c5503c0cc9f1fd3026af3028b511b0c296
dhaene.alexandre@gmail.com <dhaene.alexandre@gmail.com>
2020-06-27 12:47:24 +0200

    correctif erreur messages

--- database/migrations/2000_09_21_163209_create_photos_table.php
+++ database/migrations/2000_09_21_163209_create_photos_table.php
@@ -15,7 +15,7 @@ class CreatePhotosTable extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->longtext('name');
+            $table->string('name')->default('defaut.png');
             $table->timestamps();
         });
 
