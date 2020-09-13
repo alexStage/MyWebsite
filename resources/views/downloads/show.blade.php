e53a0961111a9e332c0878cead5dfba3ea86afc6
Alex <dhaene.alexandre@gmail.com>
2020-09-13 17:54:28 +0200

    mulan

--- resources/views/downloads/show.blade.php
+++ resources/views/downloads/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="container-fluid text-center">
-    <a href="{{asset('storage/downloads/mulan.jpg')}}" download="mulan.mkv"><img src="{{asset('storage/downloads/affiche_mulan.webp')}}" width="210" height="280"></a>
+    <a href="{{asset('storage/downloads/mulan.mkv')}}" download="mulan.mkv"><img src="{{asset('storage/downloads/affiche_mulan.webp')}}" width="210" height="280"></a>
 </div>
 
 
