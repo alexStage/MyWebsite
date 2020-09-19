bf1b0d793216ce45e1fc61ff0d6d8ae540cd9e88
Alex <dhaene.alexandre@gmail.com>
2020-09-19 22:55:16 +0200

    rectification downloads

--- resources/views/downloads/show.blade.php
+++ resources/views/downloads/show.blade.php
@@ -3,7 +3,7 @@
 
 @foreach($files as $file)
     <div class="container-fluid text-center">
-        <a href="{{asset($file)}}" download="{{$file}}">{{$file}}</a>
+        <a href="{{asset($file)}}" download="{{Storage::disk('downloads')->url($file)}}">{{$file}}</a>
     </div>
 @endforeach
 
