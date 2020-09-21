e1329a0098ee03a5a3447182742884cf7ad09665
Alex <dhaene.alexandre@gmail.com>
2020-09-21 22:15:12 +0200

    changement navbar + languer fr

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -16,69 +16,67 @@
     <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <a class="navbar-brand" target="_self" href="#">
-            <img src="{{asset('assets/logo-laeti.jpg')}}" width="60" height="60" alt=""> Manava
-        </a>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                @auth
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" target="_self" href="{{route('messages.index')}}">Messages</a>
-                        </li>
+            <div class="navbar-header">
+                <ul class="nav navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse navbar-header" id="navbarTogglerDemo01">
                         <li class="nav-item active">
                             <a class="nav-link" target="_self" href="{{route('albums.index')}}">Albums</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" target="_self" href="{{route('downloads')}}">Téléchargements</a>
-                        </li>
-                        @if(Auth::user()->isFamily())
-                        <li class="nav-item active">
-                            <a class="nav-link" target="_self" href="{{route('archives.index')}}">Archives</a>
-                        </li>
-                        @endif
-                    </ul>   
-                @endauth
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @if (Route::has('register'))
-                            <li>
-                                <a class="nav-link" target="_self" href="{{ route('register') }}">{{ __('S\'enregistrer') }}</a>
+                        @auth
+                            <li class="nav-item active">
+                                <a class="nav-link" target="_self" href="{{route('messages.index')}}">Messages</a>
                             </li>
-                    @endif
-                    @guest
-                        <li>
-                            <a class="nav-link" target="_self" href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span>{{ __('Se connecter') }}</a>
-                        </li>
-                    @else
-
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Se déconnecter') }}
-                                </a>
-                                <a class="dropdown-item" href="{{route('profile.photo')}}">
-                                    {{ __('ajouter photo de profile') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
+                            <li class="nav-item active">
+                                <a class="nav-link" target="_self" href="{{route('downloads')}}">Téléchargements</a>
+                            </li>
+                            @if(Auth::user()->isFamily())
+                                <li class="nav-item active">
+                                    <a class="nav-link" target="_self" href="{{route('archives.index')}}">Archives</a>
+                                </li>
+                            @endif
+                        @endauth
+                    </div>
                 </ul>
             </div>
-            
+
+                    <a class="navbar-brand mx-auto" target="_self" href="#"><img src="{{asset('assets/seaTheWorld2.png')}}" width="164" height="105" alt=""></a>
+
+            <ul class="navbar-nav ml-auto navbar-right">
+                <!-- Authentication Links -->
+                @if (Route::has('register'))
+                        <li>
+                            <a class="nav-link" target="_self" href="{{ route('register') }}">{{ __('S\'enregistrer') }}</a>
+                        </li>
+                @endif
+                @guest
+                    <li>
+                        <a class="nav-link" target="_self" href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span>{{ __('Se connecter') }}</a>
+                    </li>
+                @else
+
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->name }} <span class="caret"></span></a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Se déconnecter') }}
+                            </a>
+                            <a class="dropdown-item" href="{{route('profile.photo')}}">
+                                {{ __('ajouter photo de profile') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>                
     </nav>
 
 
