bd7d2e034e736fb9f173a731759c12ea8411ee49
Alex <dhaene.alexandre@gmail.com>
2020-11-27 09:31:59 +0100

    tests

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.layout')
 @section('content')
 <div id="app">
-<users-list :data-users={!!json_encode($users)!!}></users-list>
+<users-list :data-users={{json_encode($users)}}></users-list>
 </div>
 <script src="js/app.js"></script>
-
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
 @stop
