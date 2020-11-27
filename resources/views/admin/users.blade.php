66c412a6b070454a7e755ecbebd82e43b719179f
Alex <dhaene.alexandre@gmail.com>
2020-11-27 09:27:20 +0100

    tests

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -4,14 +4,5 @@
 <users-list :data-users={!!json_encode($users)!!}></users-list>
 </div>
 <script src="js/app.js"></script>
-<script>
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
-</script>
+
 @stop
