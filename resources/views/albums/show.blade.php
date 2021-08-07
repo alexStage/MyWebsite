cf465330961d3a1e23e84acb83c75a4571119d2e
Alexandre <dhaene.alexandre@gmail.com>
2021-08-07 11:30:16 +1100

    fix bug album

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -53,7 +53,7 @@
                   <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                       <a class="lightbox" href="{{asset($photo->slug)}}">
-                          <img src="{{asset($photo->slug)}}" alt="Bridge">
+                          <img src="{{asset($photo->slug)}}" class="card-img-top" alt="Bridge">
                       </a>
                     </div>
                   </div>
